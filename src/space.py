137c5b8bc788dce64291e59d9d12eb89e2f98c4c
Andrés Uribe Stengel <andres.uribe_stengel@hotmail.com>
2021-08-09 16:03:48 +0200

    Implement the prevention of two planets being created with the same coordinates.

--- src/space.py
+++ src/space.py
@@ -25,12 +25,12 @@ class Space:
 
         # Set and generate the amount of specified planets in space
         self.n_planets = n_planets
-        self.planets = self.set_planets()
+        self.planets = self.create_planets()
 
         # Informational print
         print('## Space has been created\n')
 
-    def set_planets(self):
+    def create_planets(self):
         """
         Generate the specified amount of planets in space.
 
@@ -43,13 +43,32 @@ class Space:
 
         # For the amount of specified planets, generate a planet with random coordinates (in the bounds of space)
         for cnt in range(0, self.n_planets):
-            planet_coordinates = np.random.randint(low=1, high=self.max_coordinate, size=2)
+            # Generate the coordinates
+            planet_coordinates = self.set_planet_coordinates(planets=planets)
 
+            # Create the planet
             planet = Planet(planet_coordinates)
 
+            # Add it to the array of planets
             planets[cnt] = planet.coordinates
 
         # Informational print
         print('All planet coordinates:\n', planets, '\n')
 
         return planets
+
+    def set_planet_coordinates(self, planets):
+        """
+        Generate and check if planet coordinates already exist, if they do, re-generate the coordinates.
+
+        :param planets: (numpy array) The current list of generated planets.
+        :return: (numpy array) The new planet's coordinates.
+        """
+        # Generate random coordinates
+        planet_coordinates = np.random.randint(low=1, high=self.max_coordinate, size=2)
+
+        # If the coordinates are already in the array of planets, re-generate the random coordinates
+        if any(np.equal(planets, planet_coordinates).all(1)):
+            self.set_planet_coordinates(planets=planets)
+
+        return planet_coordinates
